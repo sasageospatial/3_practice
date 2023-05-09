64225300bcbd6b26ff4040beed3056e64abda6ad
sasageospatial <115501915+sasageospatial@users.noreply.github.com>
2023-05-09 17:08:37 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,7 @@
+let map = L.map('map').setView([58.373523, 26.716045], 12)
+const osm =
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+ maxZoom: 19,
+ attribution: 'OpenStreetMap contributors',
+});
+osm.addTo(map)
