04e6e007694e5316c773fe1321b67fc62fed9209
sasageospatial <115501915+sasageospatial@users.noreply.github.com>
2023-05-10 18:55:42 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
 let map = L.map('map').setView([58.373523, 26.716045], 12)
 const osm =
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
  maxZoom: 19,
  attribution: 'OpenStreetMap contributors',
 });
 osm.addTo(map)
+
+//https://sasageospatial.github.io/3_practice/geojson/tartu_city_celltowers_edu.geojson
+// add geoJSON polygons layer*
+
+
+async function addDistrictsGeoJson(url) {
+ const response = await fetch(url)
+ const data = await response.json()
+ const polygons = L.geoJson(data)
+ polygons.addTo(map)
+}
+addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
